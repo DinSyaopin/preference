dbba60d6a295c5cd6264a7accc1a03950126086f
Dmitry Tiomkin <dmitry.tiomkin@gmail.com>
2018-03-05 00:10:39 +0400

    Correct Card class. Put here instances of Suits,Ranks enums and constructor.

--- src/com/dinsyaopin/Card.java
+++ src/com/dinsyaopin/Card.java
@@ -1,27 +1,29 @@
 package com.dinsyaopin;
 
-public class Card<Integer, String> {
-    public Integer valueOfCard;
-    public String cardSuit;
+public class Card {
+    public Suits suit;
+    public Ranks rank;
 
-    public Integer getValueOfCard() {
-        return valueOfCard;
-    }
-
-    public void setValueOfCard(Integer valueOfCard) {
-        this.valueOfCard = valueOfCard;
-    }
-
-    public Card(Integer valueOfCard, String cardSuit) {
-        this.valueOfCard = valueOfCard;
-        this.cardSuit = cardSuit;
-    }
-
-    public String getCardSuit() {
-        return cardSuit;
-    }
-
-    public void setCardSuit(String cardSuit) {
-        this.cardSuit = cardSuit;
+    public Card(int cardSuit, int cardRank) {
+        switch (cardSuit) {
+            case 0 : suit = Suits.HEARTS; break;
+            case 1 : suit = Suits.DIAMONDS; break;
+            case 2 : suit = Suits.CLUBS; break;
+            case 3 : suit = Suits.SPADES; break;
+            default: suit = null;
+                System.out.println("Incompatible suit.");
+        }
+        switch (cardRank) {
+            case 7 : rank = Ranks.SEVEN; break;
+            case 8 : rank = Ranks.EIGHT; break;
+            case 9 : rank = Ranks.NINE; break;
+            case 10 : rank = Ranks.TEN; break;
+            case 11 : rank = Ranks.JACK; break;
+            case 12 : rank = Ranks.QUEEN; break;
+            case 13 : rank = Ranks.KING; break;
+            case 14 : rank = Ranks.ACE; break;
+            default: rank = null;
+                System.out.println("Incompatible rank.");
+        }
     }
 }
